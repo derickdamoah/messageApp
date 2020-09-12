32887d9a100d7def84fcce9b3f28c2b30bb41f5f
derick damoah <derickdamoah98@gmail.com>
2020-09-12 22:05:02 +0100

    fixed errors

--- message/views.py
+++ message/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from twilio.rest import Client
+from twilio.twiml.messaging_response import Message, MessagingResponse
+from twilio import twiml
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from . import views
+
 from django_twilio.decorators import twilio_view
 from twilio.twiml.messaging_response import MessagingResponse
 import os
@@ -14,6 +20,8 @@ def home(request):
 
 def message(request):
     return render(request, "message.html",)
+
+messageSID = ""
 
 def sendMessage(request):
     try:
@@ -35,19 +43,20 @@ def sendMessage(request):
         body = message.body
         sentMessage = "Sent From: " + sender + "\n" + "Sent To: " + receiver + "\n" + "\n" + \
                       "Message Body: " + body
+
         return render(request, "sentMessages.html", {"messages":sentMessage})
 
     except:
         return render(request, "message_not_sent.html", {"messages": "Your message was not sent please check your number and try again"})
 
-messageSID = ""
 
 def reply(request):
     message = client.messages(messageSID).fetch()
     sender = message.from_
     receiver = message.to
     body = message.body
-    sentMessage = "Sent From: " + sender + "\n" + "Sent To: " + receiver + "\n" + "\n" + "Message Body: " + body
+    sentMessage = "Sent From: " + sender + "\n" + "Sent To: " + receiver + "\n" + "\n" + \
+                  "Message Body: " + body
 
     allMessages = client.messages.list()
     lis = []
@@ -62,15 +71,16 @@ def reply(request):
                 reply_to = get_message_info.to
                 message_reply = "Reply:\n" + "Sent From: " + reply_from + "\n" + "Sent To: " \
                                 + reply_to + "\n" + "\n" + "Message Body: " + reply_body
-                return render(request, "reply.html", {"reply": message_reply, "messages":body})
+
+                return render(request, "reply.html", {"reply": message_reply, "messages":sentMessage})
 
         elif message.direction == 'outbound-api':
             message_reply = "This person is yet to reply"
-            return render(request, "reply.html", {"reply": message_reply, "messages": body})
+            return render(request, "reply.html", {"reply": message_reply, "messages": sentMessage})
 
         else:
             message_reply = "This person is yet to reply"
-            return render(request, "reply.html", {"reply": message_reply, "messages": body})
+            return render(request, "reply.html", {"reply": message_reply, "messages": sentMessage})
 
 @twilio_view
 def sms_response(request):
