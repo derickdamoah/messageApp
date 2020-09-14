2053b187d9b746b4d009ec483ff6ab5df8f40b11
derick damoah <derickdamoah98@gmail.com>
2020-09-14 22:21:04 +0100

    debug to false

--- messageApp/settings.py
+++ messageApp/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd06n_0h+ii!h-*de#5yf2_etectnagbw_v&w+kh((p5#5dr3t_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "herokuapp.com",
